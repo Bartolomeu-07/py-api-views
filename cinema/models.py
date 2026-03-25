8f8362fec985c43526a625b1e058b34f8ad1fee9
Bartolomeu <bartoszokroj@wp.pl>
2026-03-25 16:26:54 +0100

    Fix issues

--- cinema/models.py
+++ cinema/models.py
@@ -14,8 +14,8 @@ class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     duration = models.IntegerField()
-    actors = models.ManyToManyField(Actor, blank=True)
-    genres = models.ManyToManyField(Genre, blank=True)
+    actors = models.ManyToManyField(Actor, blank=True, related_name="movies")
+    genres = models.ManyToManyField(Genre, blank=True, related_name="movies")
 
     def __str__(self):
         return self.title
